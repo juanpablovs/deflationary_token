26d5f817034cf93293f38bb9ce46e2fe1021da3b
juanpablovs <jpvazquez.mba2004@alumni.ie.edu>
2023-03-11 13:19:51 +0100

    imports Ownable

--- contracts/Defl.sol
+++ contracts/Defl.sol
@@ -3,7 +3,12 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Defl {
+contract Defl is Ownable{
+
+    using SafeMath for uint256;
+
+
 
 }
