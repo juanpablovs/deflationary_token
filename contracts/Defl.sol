1de7d927fa0c3785e5a345cc78512fba30069994
juanpablovs <jpvazquez.mba2004@alumni.ie.edu>
2023-03-11 14:05:53 +0100

    completed ERC20PresetMinterRebaser.sol

--- contracts/Defl.sol
+++ contracts/Defl.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "contracts/ERC20PresetMinterRebaser.sol";
 
-contract Defl is Ownable{
+contract Defl is Ownable, ERC20PresetMinterRebaser{
 
     using SafeMath for uint256;
 
