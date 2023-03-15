951cb785758d6d9cbd46e1a106fdbc1f4e6cd281
juanpablovs <jpvazquez.mba2004@alumni.ie.edu>
2023-03-15 16:57:04 +0100

    added INeuronsTokenSwap.sol

--- contracts/NeuronChef.sol
+++ contracts/NeuronChef.sol
@@ -12,5 +12,6 @@ import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./interfaces/INeuronsTokenSwap.sol";
 
 contract NeuronChef {}
--- contracts/interfaces/INeuronsTokenSwap.sol
+++ contracts/interfaces/INeuronsTokenSwap.sol
@@ -2,19 +2,25 @@
 
 pragma solidity ^0.8.0;
 
-interface INeuronsToken {
+interface INeuronsTokenSwap {
 
     function mint(address to, uint256 amount) external;
 
+    function rebase(
+        uint256 epoch,
+        uint256 indexDelta,
+        bool positive
+    ) external returns (uint256);
+
     function totalSupply() external view returns (uint256);
 
-    function transferUnderlying(address to, uint256 value) external returns (bool);
+    function transferUnderlying(address to, uint256 value)
+        external
+        returns (bool);
 
     function fragmentToNeurons(uint256 value) external view returns (uint256);
 
     function neuronsToFragment(uint256 neurons) external view returns (uint256);
 
     function balanceOfUnderlying(address who) external view returns (uint256);
-
-    function burn(uint256 amount) external;
 }
